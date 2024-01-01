3f8100447e6e9d512c9704be1c4158a4c146843d
Danish Notes <149946688+muhamadakmal1@users.noreply.github.com>
2024-01-01 11:38:01 +0530

    python Simple claculator.py

--- Simple claculator.py
+++ Simple claculator.py
@@ -1,7 +1,7 @@
 operator = input("Enter an operator (+ - * /): ")
 num1 = float(input("Enter the 1st number: "))
 num2 = float(input("Enter the 2nd number: "))
-try
+
 if operator == "+":
     result = num1 + num2
     print(round(result, 3))
